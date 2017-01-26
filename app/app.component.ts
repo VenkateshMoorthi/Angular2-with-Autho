cb79c0a9d15c0f5b2c8f1a201f6bbaf845d1cb8e
VenkateshMoorthi <venkateshmoorthi11@gmail.com>
2017-01-26 10:35:27 +0530

    Updating the user's profile

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component} from '@angular/core';
-import { Auth } from './auth.service'
+import { Auth } from './auth.service';
+import { AuthHttp } from 'angular2-jwt';
+
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,24 @@ import { Auth } from './auth.service'
 export class AppComponent {
   title = 'app works!';
 
-  constructor( private auth:Auth ){
+  constructor( private auth:Auth, private authHttp:AuthHttp ){
 
   }
 
   showProfile(){
     console.log(this.auth.userProfile);
+  }
+
+  updateProfile(){
+    var url='https://'+'moorthi.auth0.com'+'/api/v2/users/'+this.auth.userProfile.user_id; 
+    var data={
+      user_metadata:{
+        location:'Chennai'
+      }
+    }
+    this.authHttp.patch(url,data)
+      .subscribe(res=>{
+        console.log(res.json());
+      });
   }
 }
